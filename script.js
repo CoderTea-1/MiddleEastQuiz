db15ea52b1cfd9b929d9977e86c088c7cdad2088
Darmin T. <61888777+CoderTea-1@users.noreply.github.com>
2026-01-20 03:25:49 +0000

    Locations

--- script.js
+++ script.js
@@ -6,30 +6,85 @@ let score = 0;
 // -----------------
 const LOCATIONS = [
   // Countries
+  { name: "Afghanistan", type: "country", countryName: "Afghanistan" },
+  { name: "Albania", type: "country", countryName: "Albania" },
+  { name: "Armenia", type: "country", countryName: "Armenia" },
+  { name: "Azerbaijan", type: "country", countryName: "Azerbaijan" },
+  { name: "Bahrain", type: "country", countryName: "Bahrain" },
+  { name: "Bulgaria", type: "country", countryName: "Bulgaria" },
+  { name: "China", type: "country", countryName: "China" },
+  { name: "Cyprus", type: "country", countryName: "Cyprus" },
+  { name: "Djibouti", type: "country", countryName: "Djibouti" },
+  { name: "Egypt", type: "country", countryName: "Egypt" },
+  { name: "Eritrea", type: "country", countryName: "Eritrea" },
+  { name: "Ethiopia", type: "country", countryName: "Ethiopia" },
+  { name: "Georgia", type: "country", countryName: "Georgia" },
+  { name: "Greece", type: "country", countryName: "Greece" },
   { name: "India", type: "country", countryName: "India" },
+  { name: "Iran", type: "country", countryName: "Iran" },
+  { name: "Iraq", type: "country", countryName: "Iraq" },
+  { name: "Israel", type: "country", countryName: "Israel" },
+  { name: "Jordan", type: "country", countryName: "Jordan" },
+  { name: "Kazakhstan", type: "country", countryName: "Kazakhstan" },
+  { name: "Kirgizstan", type: "country", countryName: "Kyrgyzstan" },
+  { name: "Kuwait", type: "country", countryName: "Kuwait" },
+  { name: "Lebanon", type: "country", countryName: "Lebanon" },
+  { name: "Libya", type: "country", countryName: "Libya" },
+  { name: "Macedonia", type: "country", countryName: "North Macedonia" },
+  { name: "Oman", type: "country", countryName: "Oman" },
+  { name: "Pakistan", type: "country", countryName: "Pakistan" },
+  { name: "Qatar", type: "country", countryName: "Qatar" },
+  { name: "Romania", type: "country", countryName: "Romania" },
+  { name: "Russia", type: "country", countryName: "Russia" },
+  { name: "Saudi Arabia", type: "country", countryName: "Saudi Arabia" },
+  { name: "Serbia", type: "country", countryName: "Serbia" },
+  { name: "Somalia", type: "country", countryName: "Somalia" },
+  { name: "Sudan", type: "country", countryName: "Sudan" },
+  { name: "Syria", type: "country", countryName: "Syria" },
+  { name: "Tajikistan", type: "country", countryName: "Tajikistan" },
   { name: "Turkey", type: "country", countryName: "Turkey" },
+  { name: "Turkmenistan", type: "country", countryName: "Turkmenistan" },
+  { name: "Ukraine", type: "country", countryName: "Ukraine" },
+  { name: "United Arab Emirates", type: "country", countryName: "United Arab Emirates" },
+  { name: "Uzbekistan", type: "country", countryName: "Uzbekistan" },
+  { name: "Yemen", type: "country", countryName: "Yemen" },
 
-  // Seas (polygon)
-  { name: "Black Sea", type: "sea", geometry: {
-      "type": "Polygon",
-      "coordinates": [[[27, 40],[41, 40],[41, 46],[27, 46],[27,40]]]
-  }},
-  { name: "Persian Gulf", type: "sea", geometry: {
-      "type": "Polygon",
-      "coordinates": [[[48.5, 24],[56, 24],[56, 30],[48.5,30],[48.5,24]]]
-  }},
+  // Seas (polygon examples)
+  { name: "Black Sea", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[27,40],[41,40],[41,46],[27,46],[27,40]]] } },
+  { name: "Caspian Sea", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[47,36],[54,36],[54,47],[47,47],[47,36]]] } },
+  { name: "Mediterranean Sea", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[-6,30],[36,30],[36,46],[-6,46],[-6,30]]] } },
+  { name: "Indian Ocean", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[20,-40],[120,-40],[120,30],[20,30],[20,-40]]] } },
+  { name: "Red Sea", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[32,12],[43,12],[43,30],[32,30],[32,12]]] } },
+  { name: "Persian Gulf", type: "sea", geometry: { "type": "Polygon", "coordinates":[[[48.5,24],[56,24],[56,30],[48.5,30],[48.5,24]]] } },
 
-  // Rivers (approx lat/lon)
-  { name: "Nile", type: "river", lat: 26, lon: 31 },
+  // Rivers (approximate lat/lon)
   { name: "Amu Darya", type: "river", lat: 41, lon: 63 },
+  { name: "Syr Darya", type: "river", lat: 41, lon: 67 },
+  { name: "Indus", type: "river", lat: 28, lon: 70 },
+  { name: "Euphrates", type: "river", lat: 33, lon: 43 },
+  { name: "Tigris", type: "river", lat: 33, lon: 44 },
+  { name: "Nile", type: "river", lat: 26, lon: 31 },
 
   // Cities
+  { name: "Aden", type: "city", lat: 12.8, lon: 45 },
+  { name: "Herat", type: "city", lat: 34.3, lon: 62 },
   { name: "Istanbul", type: "city", lat: 41, lon: 29 },
   { name: "Mecca", type: "city", lat: 21.4, lon: 39.8 },
+  { name: "Medina", type: "city", lat: 24.5, lon: 39.6 },
 
   // Mountains
-  { name: "Himalayas", type: "mountain", lat: 28, lon: 85 }
+  { name: "Himalayas", type: "mountain", lat: 28, lon: 85 },
+  { name: "Hindu Kush", type: "mountain", lat: 35, lon: 69 },
+  { name: "Tien Shan", type: "mountain", lat: 42, lon: 83 },
+
+  // Strait / Canal locations
+  { name: "Suez Canal", type: "city", lat: 30.6, lon: 32.3 },
+  { name: "Bab el-Mandeb", type: "city", lat: 12.6, lon: 43.3 },
+  { name: "Hormuz", type: "city", lat: 26.6, lon: 56.3 },
+  { name: "Bosphorus", type: "city", lat: 41.1, lon: 29.0 },
+  { name: "Dardanelles", type: "city", lat: 40.1, lon: 26.4 }
 ];
+
 
 // -----------------
 // Initialize Map
